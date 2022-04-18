53aa9d041fa508a5644d99fe490f563e77e6e950
klinkonskydev <klinkonskydev@gmail.com>
2022-04-18 09:34:02 -0300

    feat: mui typography

--- src/styles/material-ui-theme.ts
+++ src/styles/material-ui-theme.ts
@@ -1,6 +1,21 @@
 import { createTheme } from "@mui/material/styles";
 
 const theme = createTheme({
+  typography: {
+    fontFamily: [
+      'Inter',
+      '-apple-system',
+      'BlinkMacSystemFont',
+      '"Segoe UI"',
+      'Roboto',
+      '"Helvetica Neue"',
+      'Arial',
+      'sans-serif',
+      '"Apple Color Emoji"',
+      '"Segoe UI Emoji"',
+      '"Segoe UI Symbol"',
+    ].join(','),
+  },
   palette: {
     primary: {
       main: "#E6B13B"
