2463f15302789751e0d593ec1384448d677ef003
klinkonskydev <klinkonskydev@gmail.com>
2022-04-18 06:58:18 -0300

    feat: theme provider for Material Ui

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,17 @@
 import type { AppProps } from 'next/app'
 import NextNProgress from 'nextjs-progressbar'
+
+import { ThemeProvider as MUIThemeProvider } from '@mui/material/styles'
 import { ThemeProvider } from 'styled-components'
 import { GlobalStyles } from 'styles/global'
 
+import MUITheme from 'styles/material-ui-theme'
 import theme from 'styles/theme'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider theme={theme}>
+      <MUIThemeProvider theme={MUITheme}>
       <GlobalStyles />
       <Component {...pageProps} />
       <NextNProgress
@@ -17,6 +21,7 @@ export default function App({ Component, pageProps }: AppProps) {
         height={3}
         showOnShallow={true}
       />
+      </MUIThemeProvider>
     </ThemeProvider>
   )
 }
