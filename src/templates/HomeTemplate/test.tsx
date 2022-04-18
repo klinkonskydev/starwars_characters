850633f16b2ffc862caec9837efba16611b24088
klinkonskydev <klinkonskydev@gmail.com>
2022-04-18 07:57:22 -0300

    feat: testes

--- src/templates/HomeTemplate/test.tsx
+++ src/templates/HomeTemplate/test.tsx
@@ -1,23 +1,42 @@
 import { screen } from '@testing-library/react'
 import { render } from 'utils/test-utils'
+import { CharactersResponse } from 'types'
 
 import HomeTemplate from '.'
 
 jest.mock('components/Header', () => ({
   __esModule: true,
-  default: () => <div data-testid="mock CardList" />
+  default: () => <div data-testid="mock Header" />
 }))
 
 jest.mock('components/CardList', () => ({
   __esModule: true,
-  default: () => <div data-testid="mock Header" />
+  default: () => <div data-testid="mock CardList" />
 }))
+
+jest.mock('components/CardList/skeleton', () => ({
+  __esModule: true,
+  default: () => <div data-testid="mock CardListSkeleton" />
+}))
+
+jest.mock('components/NavigateBar', () => ({
+  __esModule: true,
+  default: () => <div data-testid="mock NavigateBar" />
+}))
+
+const items: CharactersResponse = {
+  count: 0,
+  results: [],
+  previous: '',
+  next: ''
+}
 
 describe('<HomeTemplate />', () => {
   it('should render the heading', () => {
-    render(<HomeTemplate />)
+    render(<HomeTemplate characters={items} />)
 
     expect(screen.getByTestId(/mock header/i)).toBeInTheDocument()
-    expect(screen.getByTestId(/mock cardlist/i)).toBeInTheDocument()
+    expect(screen.getByTestId(/mock CardList/i)).toBeInTheDocument()
+    expect(screen.getByTestId(/mock NavigateBar/i)).toBeInTheDocument()
   })
 })
