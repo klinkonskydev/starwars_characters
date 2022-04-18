dcd778b4eb09a8a4e65bafd3c7dbf4927544fc81
klinkonskydev <klinkonskydev@gmail.com>
2022-04-18 07:56:32 -0300

    feat - CardList & HomeTemplate test

--- src/components/CardList/styles.ts
+++ src/components/CardList/styles.ts
@@ -3,14 +3,19 @@ import * as CardStyles from 'components/Card/styles'
 
 export const Wrapper = styled.div`
   ${({ theme }) => css`
-    width: 100%;
-    display: grid;
-    grid-template-columns: repeat(auto-fill, minmax(30rem, 1fr));
-    grid-template-rows: 1fr;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
     gap: ${theme.spacings.normal};
 
     ${CardStyles.Wrapper} {
       width: 100%;
+    }
+
+    @media (min-width: 550px) {
+      display: grid;
+      grid-template-columns: repeat(auto-fill, minmax(30rem, 1fr));
+      grid-template-rows: auto;
     }
 
     & a {
