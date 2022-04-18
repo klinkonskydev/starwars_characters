cfd44564a9b393c6d1c8506d2eb4c01f951660b8
klinkonskydev <klinkonskydev@gmail.com>
2022-04-18 07:48:51 -0300

    feat - CardList & HomeTemplate tests

--- src/components/CardList/mock.ts
+++ src/components/CardList/mock.ts
@@ -1,25 +1,30 @@
-const mock = {
+import { CharactersResponse } from 'types'
+
+const mock: CharactersResponse = {
+  count: 80,
   results: [
     {
       name: 'matheus',
       homeworld: 'terra',
       gender: 'male',
+      url: 'htts://mock',
       eye_color: 'blue'
     },
     {
       name: 'klinkonsky',
       homeworld: 'marte',
       gender: 'male',
+      url: 'htts://mock',
       eye_color: 'blue'
     },
     {
       name: 'Igor',
       homeworld: 'plutão',
+      url: 'htts://mock',
       gender: 'male',
       eye_color: 'blue'
     }
   ],
-  url: 'htts://mock',
   next: '1',
   previous: '3'
 }
